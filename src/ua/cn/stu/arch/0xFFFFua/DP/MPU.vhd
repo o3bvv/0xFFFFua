3a77147fb575db5456fd83df128980860cc2bcb5
Alexander Oblovatniy <oblovatniy@gmail.com>
2012-01-24 21:10:09 +0200

    Multiply unit description modified: it really operates with signed numbers in additional code.

--- src/ua/cn/stu/arch/0xFFFFua/DP/MPU.vhd
+++ src/ua/cn/stu/arch/0xFFFFua/DP/MPU.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Multiply unit
--- Operates with unsigned numbers
+-- Operates with signed numbers
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
